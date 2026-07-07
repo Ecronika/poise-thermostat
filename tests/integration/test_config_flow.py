673e4608f8c116ac48e1a7cbb7154564bb4760e7
Ecronika <112566758+Ecronika@users.noreply.github.com>
2026-07-07 12:48:37 +0200

    Add files via upload

--- tests/integration/test_config_flow.py
+++ tests/integration/test_config_flow.py
@@ -7,7 +7,8 @@ from unittest.mock import patch
 
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
-from homeassistant.helpers import device_registry as dr, entity_registry as er
+from homeassistant.helpers import device_registry as dr
+from homeassistant.helpers import entity_registry as er
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
 from custom_components.poise.const import (
